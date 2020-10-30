a0143ba79fcd1aeb6f89507eb9533aa8a71a5021
Módis Imre <hello@modisimre.hu>
2020-10-30 12:10:27 +0100

    Remove Mweb

--- src/LaravelModuleInstaller.php
+++ src/LaravelModuleInstaller.php
@@ -65,7 +65,7 @@ class LaravelModuleInstaller extends LibraryInstaller
             throw new \Exception($this->usage());
         }
 
-        return ucfirst($split[0]).implode('',array_map('ucfirst', $splitNameToUse));
+        return ucfirst(implode('',array_map('ucfirst', $splitNameToUse));
     }
 
     /**
